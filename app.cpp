08ca2d4a65ff40c58f97fbb3253c5c0379465417
Igor <igoranik86@gmail.com>
2024-12-02 10:07:20 +0200

    Add App Icon Initialization and Update Git Ignore Rules

--- app.cpp
+++ app.cpp
@@ -3,7 +3,12 @@
 
 bool App::OnInit()
 {
+    wxInitAllImageHandlers();
+
+    wxIcon appIcon("icon.ico", wxBITMAP_TYPE_ICO);
+
     MainFrame* frame = new MainFrame("CodeAtlas", wxDefaultPosition, wxSize(640, 480));
+    frame->SetIcons(wxIconBundle(appIcon));
     frame->Show(true);
     return true;
 }
